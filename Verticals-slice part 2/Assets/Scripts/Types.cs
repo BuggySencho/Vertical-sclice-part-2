d37d2de4ff103c8998e28f966b3b275e4eb434f8
ihartog <26035@ma-web.nl>
2019-12-19 15:37:15 +0100

    scene updates

--- Verticals-slice part 2/Assets/Scripts/Types.cs
+++ Verticals-slice part 2/Assets/Scripts/Types.cs
@@ -1,27 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Types : MonoBehaviour
 {
     private enum Elements
     {
-      heart,
-      body,
-      skill,
-      bravery,
-      wisdom
+        heart,
+        body,
+        skill,
+        bravery,
+        wisdom
     }
 
     // Start is called before the first frame update
     void Start()
     {
-         
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
