d08fbd81e0d576ca320a52d5229c5e54f32b882c
ihartog <26035@ma-web.nl>
2019-12-22 21:20:50 +0100

    UI layout added
    UI placed, still needs functionality

---
+++ Verticals-slice part 2/Assets/Scripts/UI/UIScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    public void TimesTwo()
+    {
+        Time.timeScale = 2f;
+        Debug.Log("Gotta go fast");
+    }
+}
